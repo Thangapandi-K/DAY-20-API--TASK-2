4ecd54ed11b40bb480e9c3dd91b02891a9392861
Thangapandi-K <thangapandi.chella@gmail.com>
2024-07-02 18:59:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## PROMISE TASK (using API)
+
+- Using public availble API
+
+- Created a random joke fetch web
+
+- [Click Here]() to preview the deplyed web preview
---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+
+const API_URL = 'https://official-joke-api.appspot.com/random_joke'
+
+// getting datas from API url
+
+const joke = fetch(API_URL)
+.then((res) => res.json())
+.then((data) => {
+
+    const jokeCard = document.getElementById("joke");
+    jokeCard.innerText = data.setup;
+
+    const jokePunch = document.getElementById("puchline");
+    jokePunch.innerText = data.punchline;
+})
+
+
+
+
